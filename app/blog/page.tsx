2ae49aaaa9956bc90e17c2d3f726ce05eadf63e6
Rohan Mukherjee <roerohan@gmail.com>
2025-06-19 12:21:09 +0530

    fix: path

---
+++ app/blog/page.tsx
@@ -0,0 +1,6 @@
+import { redirect } from 'next/navigation';
+
+export default function BlogRedirect() {
+  redirect('https://blog.roerohan.com');
+  return null;
+}
--- app/blog/route.ts
+++
@@ -1,5 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  return NextResponse.redirect('https://blog.roerohan.com', 308);
-}
